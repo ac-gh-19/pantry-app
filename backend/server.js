b67560e7f638f4d7f64c2950ef539c9aed89dbd5
ac-gh-19 <chanandrew1925@gmail.com>
2026-01-10 19:06:15 -0800

    update cors to take frontend url

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
